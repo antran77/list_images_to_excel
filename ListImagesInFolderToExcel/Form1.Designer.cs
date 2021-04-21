fafd715dff44fdbb0050517154e4c73f702bffde
DESKTOP-9VD0FAI\Admin <antran.bc@gmail.com>
2021-04-21 17:06:17 +0700

    + add src

---
+++ ListImagesInFolderToExcel/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ListImagesInFolderToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxDirection = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtImgW = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtImgH = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(39, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(586, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select folder";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDo
+            // 
+            this.btnDo.BackColor = System.Drawing.SystemColors.Window;
+            this.btnDo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDo.Location = new System.Drawing.Point(39, 291);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(586, 78);
+            this.btnDo.TabIndex = 1;
+            this.btnDo.Text = "Process It !";
+            this.btnDo.UseVisualStyleBackColor = false;
+            this.btnDo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPath.Location = new System.Drawing.Point(100, 129);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(525, 30);
+            this.txtPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path:";
+            // 
+            // cbxDirection
+            // 
+            this.cbxDirection.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cbxDirection.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxDirection.FormattingEnabled = true;
+            this.cbxDirection.Items.AddRange(new object[] {
+            "Vertical",
+            "Horizontal"});
+            this.cbxDirection.Location = new System.Drawing.Point(138, 183);
+            this.cbxDirection.Name = "cbxDirection";
+            this.cbxDirection.Size = new System.Drawing.Size(147, 37);
+            this.cbxDirection.TabIndex = 4;
+            this.cbxDirection.Text = "Horizontal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Direction:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Img Width:";
+            // 
+            // txtImgW
+            // 
+            this.txtImgW.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImgW.Location = new System.Drawing.Point(153, 243);
+            this.txtImgW.Name = "txtImgW";
+            this.txtImgW.Size = new System.Drawing.Size(104, 34);
+            this.txtImgW.TabIndex = 6;
+            this.txtImgW.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(317, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Img Height:";
+            // 
+            // txtImgH
+            // 
+            this.txtImgH.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImgH.Location = new System.Drawing.Point(444, 243);
+            this.txtImgH.Name = "txtImgH";
+            this.txtImgH.Size = new System.Drawing.Size(108, 34);
+            this.txtImgH.TabIndex = 8;
+            this.txtImgH.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(348, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Spacing:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(444, 184);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 34);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(645, 396);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtImgH);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtImgW);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxDirection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "List Images Folder to Excel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxDirection;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtImgW;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtImgH;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ ListImagesInFolderToExcel/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
+using System.Threading;
+
+namespace ListImagesInFolderToExcel
+{
+    enum Direction { Vertical = 0, Horizontal};
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void DoWork(string selectedPath,int direction, int imgWidth, int imgHeight, int spacing)
+        {
+            string[] filenames = Directory.GetFiles(selectedPath, "*", SearchOption.AllDirectories);
+            //DirectoryInfo dir = new DirectoryInfo(selectedPath);
+            //FileInfo[] file = dir.GetFiles();
+            ArrayList list = new ArrayList();
+            foreach (string file2 in filenames)
+            {
+                FileInfo info = new FileInfo(file2);
+                if (info.Extension == ".jpg" || info.Extension == ".jpeg" || info.Extension == ".png")
+                {
+                    list.Add(info);
+                }
+            }
+            ExcelPackage ExcelPkg = new ExcelPackage();
+            ExcelWorksheet wsSheet1 = ExcelPkg.Workbook.Worksheets.Add("Sheet1");
+            int rowIndex = 1;
+            int colIndex = 1;
+            Random random = new Random();
+            foreach (FileInfo info in list)
+            {
+
+                using (ExcelRange Rng = wsSheet1.Cells[rowIndex, colIndex, rowIndex, colIndex])
+                {
+                    Rng.Value = info.Name;
+                    Rng.Style.Font.Size = 14;
+                    Rng.Style.Font.Bold = true;
+                    Rng.Style.Font.Italic = true;
+                    Rng.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                }
+
+                Image img = Image.FromFile(info.FullName);
+                ExcelPicture pic = wsSheet1.Drawings.AddPicture(info.Name + random.NextDouble().ToString(), img);
+                if (direction == (int)Direction.Horizontal)
+                {
+                    pic.SetPosition(rowIndex, 0, colIndex - 1, 0);
+                    wsSheet1.Column(colIndex).AutoFit();
+                }
+                else
+                {
+                    pic.SetPosition(rowIndex - 1, 0, colIndex, 0);
+                    wsSheet1.Row(rowIndex).Height = imgHeight / 1.3;
+                    wsSheet1.Column(1).AutoFit();
+                }
+                pic.SetSize(imgWidth, imgHeight);
+                rowIndex = calculateRowIndex(direction, rowIndex, spacing);
+                colIndex = calculateColIndex(direction, colIndex, spacing);
+            }
+            if (list.Count > 0)
+            {
+                wsSheet1.Protection.IsProtected = false;
+                wsSheet1.Protection.AllowSelectLockedCells = false;
+                string filename = String.Format(@"D:\ImagesReport{0}.xlsx", random.Next(100).ToString());
+                ExcelPkg.SaveAs(new FileInfo(filename));
+                MessageBox.Show("Done, output file: " + filename, "Finish", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                btnDo.Text = "Process It !";
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String selectedPath = txtPath.Text;
+            int direction = cbxDirection.SelectedIndex;
+            int imgWidth = Int32.Parse(txtImgW.Text);
+            int imgHeight = Int32.Parse(txtImgH.Text);
+            int spacing = Convert.ToInt32(numericUpDown1.Value);
+            if (selectedPath.Length == 0) return;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            btnDo.Text = "Working, please wait....";
+            Thread thread1 = new Thread(()=>DoWork(selectedPath,direction, imgWidth, imgHeight, spacing));
+            thread1.Start();
+
+        }
+
+        private int calculateColIndex(int direction, int colIndex, int spacing)
+        {
+            switch (direction)
+            {
+                case (int)Direction.Vertical:
+                    return colIndex;
+                case (int)Direction.Horizontal:
+                    return colIndex + spacing;
+            }
+            return colIndex;
+        }
+
+        private int calculateRowIndex(int direction, int rowIndex, int spacing)
+        {
+            switch (direction)
+            {
+                case (int)Direction.Vertical:
+                    return rowIndex + spacing;
+                case (int)Direction.Horizontal:
+                    return rowIndex;
+            }
+            return rowIndex;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtImgW.Text = txtImgH.Text = "100";
+        }
+    }
+}
